424e15e874aeda59f6713fc2c4319cdfd2969484
Jay <73120876+JarrellW@users.noreply.github.com>
2020-10-28 14:28:32 -0400

    tolargetosmall lab

--- TooLargeTooSmall.java
+++ TooLargeTooSmall.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class TooLargeTooSmall here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Jarrell Wells)
+ * @version (10/28/2020)
  */
 public class TooLargeTooSmall {
     private Integer chosenNum;
@@ -17,8 +17,13 @@ public class TooLargeTooSmall {
     }
     
     public Integer guess(Integer g) {
-        
-        return chosenNum;
+        if (g < chosenNum) {
+            return -1;
+        } else if (g > chosenNum) {
+            return 1;
+        } else {
+            return 0;
 
     }
+ }
 }
